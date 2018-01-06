c9cc5dd4d09ecd694a91fae6a97bba961ff3819e
ip-d <sonu_kumar@esferasoft.com>
2018-01-06 10:50:07 +0530

    fcm

--- index.php
+++ index.php
@@ -25,6 +25,46 @@
 </head>
 <body>
 
+<!-- AAAAmF2NUFE:APA91bFDaVmuSl9lNLXeRuk1Rtu3ZdspTorc0JXFvxwtoQ6qB_FNtY2JMLYxSRKHY9mz1uxpLggfWdIno-n5x3HLKzTBzOBVAaIc77kS4NWvpOKA2REQqajvjDDVB3QhPzmPK6mzA1hW -->
+
+<button onclick="sendMessageToUser()">Send Notification</button>
+
+<script type="text/javascript">
+  function sendMessageToUser()
+  {
+     $.ajax(
+     {
+         type : "POST",
+         url : "https://fcm.googleapis.com/fcm/send",
+
+         headers :
+         {
+             Authorization : "key=AAAAmF2NUFE:APA91bFDaVmuSl9lNLXeRuk1Rtu3ZdspTorc0JXFvxwtoQ6qB_FNtY2JMLYxSRKHY9mz1uxpLggfWdIno-n5x3HLKzTBzOBVAaIc77kS4NWvpOKA2REQqajvjDDVB3QhPzmPK6mzA1hW"
+         },
+
+         contentType : 'application/json',
+         data : JSON.stringify(
+         {
+             "to" : "<FCM REGISTRATION TOKEN OF THE DEVICE>",
+             "data":
+             {
+                 "action" : "set_message",
+                 "message" : <YOUR MESSAGE>,
+                 "user" : "some-user"
+             }
+         }),
+
+         success : function(response)
+         {
+             console.log(response);
+         },
+         error : function(xhr, status, error)
+         {
+             console.log(xhr.error);
+         }
+     });
+  }
+</script>
 </body>
 </html>
 
