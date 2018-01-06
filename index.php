5b7ab576cb53b77fd64d7290876acf063d2b2109
ip-d <sonu_kumar@esferasoft.com>
2018-01-06 09:42:55 +0530

    fcm

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCyvGJbZuMhevqccUlIokjLZafjFFJ4Z4A",
+    authDomain: "mrneeds-bc4b7.firebaseapp.com",
+    databaseURL: "https://mrneeds-bc4b7.firebaseio.com",
+    projectId: "mrneeds-bc4b7",
+    storageBucket: "",
+    messagingSenderId: "654404571217"
+  };
+  firebase.initializeApp(config);
+</script>
+
+<?php
+
+echo "Testing FCM";
+
+?>
