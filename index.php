2dd66f900263b3b05fab313d3147119f233cacf3
ip-d <sonu_kumar@esferasoft.com>
2018-01-06 10:43:03 +0530

    fcm

--- index.php
+++ index.php
@@ -1,48 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js"></script>
 
-<!-- <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script> -->
-<script type="text/javascript" src="https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js"></script>
+  <!-- <script type="text/javascript" src="firebase-messaging-sw.js"></script> -->
+  <script>
+    Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCyvGJbZuMhevqccUlIokjLZafjFFJ4Z4A",
+      authDomain: "mrneeds-bc4b7.firebaseapp.com",
+      databaseURL: "https://mrneeds-bc4b7.firebaseio.com",
+      projectId: "mrneeds-bc4b7",
+      storageBucket: "",
+      messagingSenderId: "654404571217"
+    };
+    firebase.initializeApp(config);
 
-<script type="text/javascript" src="firebase-messaging-sw.js"></script>
-<script>
-  // Initialize Firebase
-  // var config = {
-  //   apiKey: "AIzaSyCyvGJbZuMhevqccUlIokjLZafjFFJ4Z4A",
-  //   authDomain: "mrneeds-bc4b7.firebaseapp.com",
-  //   databaseURL: "https://mrneeds-bc4b7.firebaseio.com",
-  //   projectId: "mrneeds-bc4b7",
-  //   storageBucket: "",
-  //   messagingSenderId: "654404571217"
-  // };
-  // firebase.initializeApp(config);
+    messaging.requestPermission()
+    .then(function() {
+      console.log('Notification permission granted.');
+      // TODO(developer): Retrieve an Instance ID token for use with FCM.
+      // ...
+    })
+    .catch(function(err) {
+      console.log('Unable to get permission to notify.', err);
+    });
 
-  // messaging.requestPermission()
-  // .then(function() {
-  //   console.log('Notification permission granted.');
-  //   // TODO(developer): Retrieve an Instance ID token for use with FCM.
-  //   // ...
-  // })
-  // .catch(function(err) {
-  //   console.log('Unable to get permission to notify.', err);
-  // });
+    messaging.getToken()
+    .then((currentToken) => {
+      if (currentToken) {
+        sendTokenToServer(currentToken)
+      } else {
+        // you don't have permission to show notifications
+        // detect whether they are blocked or not, then show your custom UI  
+      }
+    })
+    .catch((err) => {
+      // retrieving token failed, analyze the error
+    })
 
-  // messaging.getToken()
-  // .then((currentToken) => {
-  //   if (currentToken) {
-  //     sendTokenToServer(currentToken)
-  //   } else {
-  //     // you don't have permission to show notifications
-  //     // detect whether they are blocked or not, then show your custom UI  
-  //   }
-  // })
-  // .catch((err) => {
-  //   // retrieving token failed, analyze the error
-  // })
+  </script>
+</head>
+<body>
 
-</script>
+</body>
+</html>
+
 
 <?php
 
