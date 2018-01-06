9fb2ad9d8aa5f60bbd940e8113f81a076ffaaf64
ip-d <sonu_kumar@esferasoft.com>
2018-01-06 10:47:12 +0530

    fcm

--- index.php
+++ index.php
@@ -2,27 +2,26 @@
 <html>
 <head>
   <title></title>
-  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
-  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script>
-  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js"></script>
-  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js"></script>
-  <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js"></script>
+  
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCyvGJbZuMhevqccUlIokjLZafjFFJ4Z4A",
+    authDomain: "mrneeds-bc4b7.firebaseapp.com",
+    databaseURL: "https://mrneeds-bc4b7.firebaseio.com",
+    projectId: "mrneeds-bc4b7",
+    storageBucket: "mrneeds-bc4b7.appspot.com",
+    messagingSenderId: "654404571217"
+  };
+  firebase.initializeApp(config);
+</script>
 
-  <!-- <script type="text/javascript" src="firebase-messaging-sw.js"></script> -->
-  <script>
-    Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyCyvGJbZuMhevqccUlIokjLZafjFFJ4Z4A",
-      authDomain: "mrneeds-bc4b7.firebaseapp.com",
-      databaseURL: "https://mrneeds-bc4b7.firebaseio.com",
-      projectId: "mrneeds-bc4b7",
-      storageBucket: "",
-      messagingSenderId: "654404571217"
-    };
-    firebase.initializeApp(config);
-
-
-  </script>
 </head>
 <body>
 
