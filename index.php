6d28b9f5633568965612c794c7e6b0aaa2a93da1
ip-d <sonu_kumar@esferasoft.com>
2018-01-06 11:12:20 +0530

    fcm

--- index.php
+++ index.php
@@ -77,9 +77,9 @@ limitations under the License.
 <!-- If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup -->
 <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
 <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js"></script>
-<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js"></script>
+<!-- <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script> -->
+<!-- <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js"></script> -->
+<!-- <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js"></script> -->
 <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js"></script>
 <script type="text/javascript">
   var config = {
