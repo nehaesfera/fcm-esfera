31fa6da6479635e4d0bca9d40aaaff32810b6813
ip-d <sonu_kumar@esferasoft.com>
2018-01-06 10:11:44 +0530

    fcm

--- index.php
+++ index.php
@@ -15,6 +15,17 @@
     messagingSenderId: "654404571217"
   };
   firebase.initializeApp(config);
+
+  messaging.requestPermission()
+  .then(function() {
+    console.log('Notification permission granted.');
+    // TODO(developer): Retrieve an Instance ID token for use with FCM.
+    // ...
+  })
+  .catch(function(err) {
+    console.log('Unable to get permission to notify.', err);
+  });
+
 </script>
 
 <?php
