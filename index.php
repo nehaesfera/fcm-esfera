f38a7676f57408023f3b4ff634a0b347246c201e
ip-d <sonu_kumar@esferasoft.com>
2018-01-06 09:50:31 +0530

    fcm

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 
 <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js"></script>
 <script>
   // Initialize Firebase
   var config = {
